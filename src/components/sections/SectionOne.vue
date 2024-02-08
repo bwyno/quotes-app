fbb8d6c3d1ad7896161e54af70b8e39f65865db4
Bryan Arnoco <arnoco42@gmail.com>
2024-02-08 23:28:32 +0800

    first commit

---
+++ src/components/sections/SectionOne.vue
@@ -0,0 +1,16 @@
+<template>
+  <section class="section-one">
+    <div>adsasd</div>
+  </section>
+</template>
+<script setup lang="ts">
+</script>
+
+<style>
+.section-one {
+  width: auto;
+  height: auto;
+  flex-shrink: 0;
+  overflow: hidden;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,7 @@
+<template>
+  <SectionOne />
+</template>
+
+<script setup lang="ts">
+import SectionOne from '@/components/sections/SectionOne.vue';
+</script>
